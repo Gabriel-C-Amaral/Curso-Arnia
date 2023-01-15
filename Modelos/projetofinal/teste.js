15f26586b4eb35ea572d9006f57ae35d0e6704ce
Gabriel-C-Amaral <gabrielcorreaamaral94@gmail.com>
2023-01-15 14:27:59 -0300

    Changes to be committed: 	deleted:    Modelos/projetofinal/teste.js 	deleted:    Modelos/projetofinal/testes.html

--- Modelos/projetofinal/teste.js
+++
@@ -1,6 +0,0 @@
-let arr = [{"taskNum":"01","description":"01","taskDate":"01/01/2001","taskStatus":"Concluído"},
-{"taskNum":"01","description":"01","taskDate":"01/01/2001","taskStatus":"Concluído"},
-{"taskNum":"01","description":"01","taskDate":"01/01/2001","taskStatus":"Concluído"},{"taskNum":"01","description":"01","taskDate":"01/01/2001","taskStatus":"Concluído"},
-{"taskNum":"01","description":"01","taskDate":"01/01/2001","taskStatus":"Concluído"}];
-let str = arr.map(JSON.stringify).join(",");
-console.log(str); // Output: {"taskNum":"01","description":"01","taskDate":"01/01/2001","taskStatus":"Concluído"}

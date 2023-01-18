295cd0f5b481958d628b12fff7adef7f369c105d
Gabriel-C-Amaral <gabrielcorreaamaral94@gmail.com>
2023-01-17 21:35:18 -0300

    Changes to be committed: 	modified:   Modulo2/Aula1701/aula1701/index.html 	deleted:    Modulo2/Aula1701/aula1701/src/App.css 	modified:   Modulo2/Aula1701/aula1701/src/App.jsx 	deleted:    Modulo2/Aula1701/aula1701/src/assets/react.svg 	deleted:    Modulo2/Aula1701/aula1701/src/index.css 	modified:   Modulo2/Aula1701/aula1701/src/main.jsx

--- Modulo2/Aula1701/aula1701/src/App.jsx
+++ Modulo2/Aula1701/aula1701/src/App.jsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
 function App() {
   const randomNumber = Math.random()
 
   return (
     <div className="App">
+      <h1>Testando</h1>
       {randomNumber > (0.5) ? <p>maior</p> : <p>menor</p>} 
     </div>
   )

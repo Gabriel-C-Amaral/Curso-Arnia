a9830727a78a88c99b84a5c513418b295b2f3ec0
Gabriel-C-Amaral <gabrielcorreaamaral94@gmail.com>
2023-01-21 20:54:24 -0300

    Changes to be committed: 	modified:   Modulo2/Aula1901/curriculo/src/components/body/body.css 	modified:   Modulo2/Aula1901/curriculo/src/components/body/body.jsx 	modified:   Modulo2/Aula1901/curriculo/src/components/branch/branch.jsx

--- Modulo2/Aula1901/curriculo/src/components/body/body.jsx
+++ Modulo2/Aula1901/curriculo/src/components/body/body.jsx
@@ -5,7 +5,11 @@ import BranchName from '../branch/branch'
 const Body = () => {
     return (
         <div className="container">
-        {BranchName(0)}
+        {BranchName("About me")}
+        <div id='textAbout'>
+        Donec a augue gravida, vulputate ligula et, pellentesque arcu. 🤖Morbi feugiat eros nec sem ultrices, et venenatis velit posuere. Donec bibendum commodo dui, eget sollicitudin urna sagittis non. Donec ac commodo tortor. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+        </div>
+        {BranchName("Education")}
       </div>    )
 }
 
--- Modulo2/Aula1901/curriculo/src/components/branch/branch.jsx
+++ Modulo2/Aula1901/curriculo/src/components/branch/branch.jsx
@@ -2,11 +2,13 @@ import Dash from '../../assets/dash.svg'
 import '../branch/branch.css'
 
 
-const branchs = ["About Me", "Education", "Experience", "Skills"]
+// const branchs = ["About Me", "Education", "Experience", "Skills"]
 
-const BranchName = (index) => {
+const BranchName = (name) => {
+    
        return (        
-       <div className='branchName'>{branchs[index]}
+       <div className='branchName'>
+        {name}
        <img src={Dash} id="dash" alt="" />
        </div>
     )

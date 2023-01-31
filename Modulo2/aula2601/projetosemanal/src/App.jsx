8035ad409a312d2314d75003775f57050f53ed01
Gabriel-C-Amaral <gabrielcorreaamaral94@gmail.com>
2023-01-30 22:32:41 -0300

    Changes to be committed: 	modified:   Modulo2/aula2601/projetosemanal/src/App.css 	modified:   Modulo2/aula2601/projetosemanal/src/App.jsx 	new file:   Modulo2/aula2601/projetosemanal/src/components/assets/db.js 	new file:   Modulo2/aula2601/projetosemanal/src/components/ecommerce/index.jsx 	new file:   Modulo2/aula2601/projetosemanal/src/components/ecommerce/styles.css 	modified:   Modulo2/aula2601/projetosemanal/src/index.css

--- Modulo2/aula2601/projetosemanal/src/App.jsx
+++ Modulo2/aula2601/projetosemanal/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Ecommerce from './components/ecommerce'
 
 function App() {
 
   return (
-    <div className="App">
-     
+    <div>
+     <Ecommerce/>     
     </div>
   )
 }
---
+++ Modulo2/aula2601/projetosemanal/src/components/ecommerce/index.jsx
@@ -0,0 +1,17 @@
+import catalog from '../assets/db'
+import './styles.css'
+
+function Ecommerce (){
+    return (
+        <div className='catalog'>
+            <div className="logo"></div>
+            <div className="title"> Loja Virtual</div>
+
+        </div>
+    )
+}
+
+
+
+
+export default Ecommerce

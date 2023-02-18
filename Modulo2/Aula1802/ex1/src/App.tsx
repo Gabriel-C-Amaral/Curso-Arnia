8e4df408df2f526ead860fe582ada7bcbfc415ee
Gabriel-C-Amaral <gabrielcorreaamaral94@gmail.com>
2023-02-18 11:26:50 -0300

    Changes to be committed: 	new file:   Modulo2/Aula1802/ex1/.gitignore 	new file:   Modulo2/Aula1802/ex1/index.html 	new file:   Modulo2/Aula1802/ex1/package-lock.json 	new file:   Modulo2/Aula1802/ex1/package.json 	new file:   Modulo2/Aula1802/ex1/public/vite.svg 	new file:   Modulo2/Aula1802/ex1/src/App.css 	new file:   Modulo2/Aula1802/ex1/src/App.tsx 	new file:   Modulo2/Aula1802/ex1/src/assets/react.svg 	new file:   Modulo2/Aula1802/ex1/src/components/quotes/index.tsx 	new file:   Modulo2/Aula1802/ex1/src/index.css 	new file:   Modulo2/Aula1802/ex1/src/main.tsx 	new file:   Modulo2/Aula1802/ex1/src/vite-env.d.ts 	new file:   Modulo2/Aula1802/ex1/tsconfig.json 	new file:   Modulo2/Aula1802/ex1/tsconfig.node.json 	new file:   Modulo2/Aula1802/ex1/vite.config.ts

---
+++ Modulo2/Aula1802/ex1/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import GenerateQuote from "./components/quotes";
+
+function App() {
+  return (
+    <div className="App">
+      <GenerateQuote />
+    </div>
+  );
+}
+
+export default App;
---
+++ Modulo2/Aula1802/ex1/src/components/quotes/index.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+
+type Quotes = {
+  _id: string;
+  // The quotation text
+  content: string;
+  // The full name of the author
+  author: string;
+  // The `slug` of the quote author
+  authorSlug: string;
+  // The length of quote (number of characters)
+  length: number;
+  // An array of tag names for this quote
+  tags: string[];
+};
+
+type Query = Quotes[];
+
+type Props = {
+  content: string;
+};
+
+const GenerateQuote: React.FC = () => {
+  const [frases, setFrases] = useState<Quotes["content"]>();
+  const [click, setClick] = useState(1);
+
+  const clicking = () => setClick(click + 1);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const apiResponse = await fetch("https://api.quotable.io/random");
+      const quote: Quotes = await apiResponse.json();
+      setFrases(quote.content);
+    };
+    fetchData();
+  }, [click]);
+
+  return (
+    <div>
+      <p>{frases}</p>
+      <button onClick={() => clicking()}>Lero lero Generator</button>
+    </div>
+  );
+};
+
+export default GenerateQuote;

5d3db3516cfb957c7b525330ff1411585ccd4dc6
Gabriel-C-Amaral <gabrielcorreaamaral94@gmail.com>
2023-03-24 13:16:21 -0300

    Changes to be committed: 	modified:   Modulo2/projeto-wexer/index.html 	modified:   Modulo2/projeto-wexer/package.json 	modified:   Modulo2/projeto-wexer/src/components/app/index.tsx 	new file:   Modulo2/projeto-wexer/src/components/teste/index.tsx 	new file:   Modulo2/projeto-wexer/src/images/logo.svg 	new file:   Modulo2/projeto-wexer/src/images/marca_white.svg 	modified:   Modulo2/projeto-wexer/yarn.lock

--- Modulo2/projeto-wexer/src/components/app/index.tsx
+++ Modulo2/projeto-wexer/src/components/app/index.tsx
@@ -1,9 +1,11 @@
 import { Normalize } from 'styled-normalize'
+import Teste from '../teste'
 
 function App() {
   return (
     <div className="App">
       <Normalize />
+      <Teste />
       <h1>oi</h1>
     </div>
   )

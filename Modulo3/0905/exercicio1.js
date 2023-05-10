cfd9d515874d6e2b59c5890fa971e876e9fc3e81
Gabriel-C-Amaral <gabrielcorreaamaral94@gmail.com>
2023-05-10 18:01:55 -0300

    Changes to be committed: 	modified:   Modulo3/0905/exercicio1.js 	modified:   Modulo3/0905/exercicio2.js

--- Modulo3/0905/exercicio1.js
+++ Modulo3/0905/exercicio1.js
@@ -3,23 +3,56 @@ const repository = [];
 // Crie uma funçao que adiciona um carro(Object) ao repository e retorna o objeto salvado
 // Parametros: modelo(string), cor(string), ano(number)
 // Returno(Objeto): id(string), modelo(string), cor(string), ano(number), createdAt(Date)
-function createCar() {}
+function createCar(modelo, cor, ano) {
+  const car = {
+    id: uuidv4(), // utilize uma biblioteca como a 'uuid' para gerar ids únicos
+    modelo,
+    cor,
+    ano,
+    createdAt: new Date(),
+  };
+  repository.push(car);
+  return car;
+}
 
 // Crie uma funçao que recebe um id por parametro e retorna o objeto referente
 // Parametros: id(string)
 // Returno(Objeto): id(string), modelo(string), cor(string), ano(number), createdAt(Date)
-function findCarById() {}
+function findCarById(id) {
+  return repository.find((car) => car.id === id);
+}
 
 // Crie uma funçao que retorna todos os carros
 // Parametros: id(string)
 // Returno(Array): (id(string), modelo(string), cor(string), ano(number), createdAt(Date))[]
-function findAllCars() {}
+function findAllCars() {
+  return repository;
+}
 
 // Crie uma funçao que atualiza um carro(Object) no repository e retorna o objeto atualizado
 // Parametros: id(string), modelo(string), cor(string), ano(number)
 // Returno(Objeto): id(string), modelo(string), cor(string), ano(number), createdAt(Date)
-function updateCarById() {}
+function updateCarById(id, modelo, cor, ano) {
+  const carIndex = repository.findIndex((car) => car.id === id);
+  if (carIndex === -1) {
+    return null; // retorna null se o carro não for encontrado
+  }
+  const updatedCar = {
+    ...repository[carIndex], // mantém todas as propriedades do carro original
+    modelo,
+    cor,
+    ano,
+  };
+  repository[carIndex] = updatedCar;
+  return updatedCar;
+}
 
 // Crie uma funçao que remove um carro do repositorio
 // Parametros: id(string)
-function deleteCarById() {}
+function deleteCarById(id) {
+  const carIndex = repository.findIndex((car) => car.id === id);
+  if (carIndex === -1) {
+    return null; // retorna null se o carro não for encontrado
+  }
+  repository.splice(carIndex, 1);
+}
--- Modulo3/0905/exercicio2.js
+++ Modulo3/0905/exercicio2.js
@@ -1,22 +1,67 @@
-// const exemploDeAluno = {
-//   code: 'uuid',
-//   name: '',
-//   turma: '401B',
-//   status: 'matriculado',
-//   document: '123.123.123-00',
-//   createdAt: '2015-04-12'
-// }
+const uuid = require("uuid"); // importa a biblioteca 'uuid'
+
+const exampleStudent = {
+  code: uuid.v4(), // utiliza a função v4 da biblioteca 'uuid' para gerar um código único
+  name: "",
+  turma: "401B",
+  status: "matriculado",
+  document: "123.123.123-00",
+  createdAt: "2015-04-12",
+};
 
 // crie uma função factory que gera um Student
 // Parametros: name, turma, status, document
-function makeStudent(data) {}
+function makeStudent(name, turma, status, document) {
+  const code = uuid.v4(); // gera um código único
+  return {
+    code,
+    name,
+    turma,
+    status,
+    document,
+    createdAt: new Date(),
+  };
+}
 
-// Crie uma classe chamada StudentRepository
-// Deve ser possível:
-// - Criar;
-// - Editar;
-// - Buscar por código;
-// - Buscar por Turma;
-// - Deletar.
+class StudentRepository {
+  constructor() {
+    this.students = [];
+  }
 
-class StudentRepository {}
+  // Cria um novo Student e adiciona ao repositório
+  createStudent(name, turma, status, document) {
+    const student = makeStudent(name, turma, status, document);
+    this.students.push(student);
+    return student;
+  }
+
+  // Edita um Student existente no repositório
+  editStudent(code, data) {
+    const index = this.students.findIndex((student) => student.code === code);
+    if (index === -1) {
+      return null; // retorna null se o Student não for encontrado
+    }
+    const updatedStudent = { ...this.students[index], ...data };
+    this.students[index] = updatedStudent;
+    return updatedStudent;
+  }
+
+  // Busca um Student por código
+  findStudentByCode(code) {
+    return this.students.find((student) => student.code === code);
+  }
+
+  // Busca todos os Students de uma determinada turma
+  findStudentsByTurma(turma) {
+    return this.students.filter((student) => student.turma === turma);
+  }
+
+  // Deleta um Student do repositório
+  deleteStudent(code) {
+    const index = this.students.findIndex((student) => student.code === code);
+    if (index === -1) {
+      return null; // retorna null se o Student não for encontrado
+    }
+    this.students.splice(index, 1);
+  }
+}
